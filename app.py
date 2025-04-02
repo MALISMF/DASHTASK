b4e6e5e5a5fc925486990c5bdb15a2c4a893f103
MALISMF <you@example.com>
2025-04-02 17:31:32 +0800

    app.py refactored

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+"""
+Основной модуль приложения Dash.
+Настраивает и запускает веб-сервер с приложением визуализации данных.
+"""
 from dash import Dash
 from layouts import create_layout
 from callbacks import register_callbacks
 
-# Подключение внешних CSS стилей
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# Константы приложения
+EXTERNAL_STYLESHEETS = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+def initialize_app() -> Dash:
+    """
+    Инициализирует приложение Dash с внешними стилями.
+    
+    Returns:
+        Dash: Настроенное приложение Dash.
+    """
+    app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
+    app.layout = create_layout()
+    register_callbacks(app)
+    return app
+
 
 # Определение серверного объекта для Gunicorn
+app = initialize_app()
 server = app.server
 
-# Создание layout
-app.layout = create_layout()
 
-# Регистрация callback-функций
-register_callbacks(app)
-
-# Запуск сервера
 if __name__ == '__main__':
-    app.run_server(debug=False)  
+    app.run_server(debug=False)
