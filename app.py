f940c1a5ba4d7134b4caa0454f8d97b846cc30b9
MALISMF <you@example.com>
2025-05-06 09:22:08 +0800

    refactor(app.py): Создание функции инициализации приложения

--- app.py
+++ app.py
@@ -9,16 +9,22 @@ from callbacks import register_callbacks
 # Подключение внешних CSS-стилей
 EXTERNAL_STYLESHEETS = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
+def initialize_app() -> Dash:
+    """
+    Инициализирует приложение Dash с внешними стилями.
+    
+    Returns:
+        Dash: Настроенное приложение Dash.
+    """
+    app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
+    app.layout = create_layout()
+    register_callbacks(app)
+    return app
 
 # Определение серверного объекта для Gunicorn
+app = initialize_app()
 server = app.server
 
-# Создание layout
-app.layout = create_layout()
-
-# Регистрация callback-функций
-register_callbacks(app)
 
 # Запуск сервера
 if __name__ == '__main__':
